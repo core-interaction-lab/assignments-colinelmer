1736145b230ed2dfcd016a0f4e98a20de098cb5a
colinelmer <elmec739@newschool.edu>
2022-05-03 19:09:22 -0400

    Project 4 Edits

--- projects/project-4/story.js
+++ projects/project-4/story.js
@@ -1,15 +1,9 @@
-console.log("Made by Andrew");
-
-function playMadLibs() {
-  //going to grab user entry and store it in variables in JavaScript
-  //document.getElementById grabs HTML ids
-  //JavaScript is insanely case sensitive
+function responses() {
   var name = document.getElementById("name").value;
   var date = document.getElementById("date").value;
   var feeling = document.getElementById("feeling").value;
   var entry = document.getElementById("entry").value;
   
-  //push out back to HTML with .innerHTML
-  document.getElementById("showStory").style.visibility = "visible";
-  document.getElementById("showStory").innerHTML = "<b>My diary entry:</b><br>" + name + "</b><br>" + date + "</b><br> Today I am feeling " + feeling + " because " + entry + "<br></b><br>Refresh the page to start a new entry</b>";
+  document.getElementById("showEntry").style.visibility = "visible";
+  document.getElementById("showEntry").innerHTML = "<b>My diary entry:</b><br>" + name + "</b><br>" + date + "</b><br> Today I am feeling " + feeling + " because " + entry + "<br></b><br>Refresh the page to start a new entry</b>";
 }
