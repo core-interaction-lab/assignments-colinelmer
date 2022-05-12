9e8a356e11612084f856a0bfff855a96fd785dc6
colinelmer <elmec739@newschool.edu>
2022-05-12 18:59:17 -0400

    Project 4

---
+++ projects/project-4/suprised.js
@@ -0,0 +1,8 @@
+function responses() {
+    var name = document.getElementById("name").value;
+    var date = document.getElementById("date").value;
+    var entry = document.getElementById("entry").value;
+    
+    document.getElementById("showEntry").style.visibility = "visible";
+    document.getElementById("showEntry").innerHTML = "<b>My diary entry:</b><br>" + name + "</b><br>" + date + "</b><br> I am feeling suprised because " + entry + "<br></b>";
+  }
