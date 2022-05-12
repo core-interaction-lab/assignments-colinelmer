faa32c2357c5c2cf85088f28636ddc6cbeb3beea
colinelmer <elmec739@newschool.edu>
2022-05-12 17:39:44 -0400

    Project 4 updates

--- projects/project-4/story.js
+++ projects/project-4/story.js
@@ -1,9 +1,8 @@
 function responses() {
   var name = document.getElementById("name").value;
   var date = document.getElementById("date").value;
-  var feeling = document.getElementById("feeling").value;
   var entry = document.getElementById("entry").value;
   
   document.getElementById("showEntry").style.visibility = "visible";
-  document.getElementById("showEntry").innerHTML = "<b>My diary entry:</b><br>" + name + "</b><br>" + date + "</b><br> Today I am feeling " + feeling + " because " + entry + "<br></b><br>Refresh the page to start a new entry</b>";
+  document.getElementById("showEntry").innerHTML = "<b>My diary entry:</b><br>" + name + "</b><br>" + date + "</b><br> I am feeling happy because " + entry + "<br></b>";
 }
